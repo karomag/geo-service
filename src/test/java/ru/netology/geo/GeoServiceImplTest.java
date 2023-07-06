6edb5a972a9d8c2bd47b65f1caf8fb6c50081f3e
karomag <karomag88@mail.ru>
2023-07-06 10:52:34 +0300

    refactored test

--- src/test/java/ru/netology/geo/GeoServiceImplTest.java
+++ src/test/java/ru/netology/geo/GeoServiceImplTest.java
@@ -6,25 +6,26 @@ import org.junit.jupiter.params.provider.*;
 import ru.netology.entity.Country;
 import ru.netology.entity.Location;
 
+import java.util.stream.Stream;
+
 public class GeoServiceImplTest {
 
+    private static Stream<Arguments> byIpTest() {
+        return Stream.of(
+                Arguments.of(GeoServiceImpl.LOCALHOST, new Location(null, null, null, 0)),
+                Arguments.of(GeoServiceImpl.MOSCOW_IP, new Location("Moscow", Country.RUSSIA, "Lenina", 15)),
+                Arguments.of(GeoServiceImpl.NEW_YORK_IP, new Location("New York", Country.USA, " 10th Avenue", 32)),
+                Arguments.of("172.", new Location("Moscow", Country.RUSSIA, null, 0)),
+                Arguments.of("96.", new Location("New York", Country.USA, null, 0))
+        );
+    }
+
     @ParameterizedTest
-    @ValueSource(strings = {GeoServiceImpl.LOCALHOST, GeoServiceImpl.MOSCOW_IP, GeoServiceImpl.NEW_YORK_IP, "172.", "96."})
-    public void byIpTest(String ip) {
+    @MethodSource //When we don't provide a name for the @MethodSource, JUnit will search for a source method with the same name as the test method.
+    public void byIpTest(String ip, Location expected) {
         GeoService geoService = new GeoServiceImpl();
         Location result = geoService.byIp(ip);
-        Location expected = null;
-        if (GeoServiceImpl.LOCALHOST.equals(ip)) {
-            expected = new Location(null, null, null, 0);
-        } else if (GeoServiceImpl.MOSCOW_IP.equals(ip)) {
-            expected = new Location("Moscow", Country.RUSSIA, "Lenina", 15);
-        } else if (GeoServiceImpl.NEW_YORK_IP.equals(ip)) {
-            expected = new Location("New York", Country.USA, " 10th Avenue", 32);
-        } else if (ip.startsWith("172.")) {
-            expected = new Location("Moscow", Country.RUSSIA, null, 0);
-        } else if (ip.startsWith("96.")) {
-            expected = new Location("New York", Country.USA, null, 0);
-        }
+
         Assertions.assertEquals(expected.getCountry(), result.getCountry());
         Assertions.assertEquals(expected.getCity(), result.getCity());
         Assertions.assertEquals(expected.getStreet(), result.getStreet());
